61cd1407804ddb695121d2b01ec79b78d03f2f30
Dan LaManna <dan.lamanna@kitware.com>
2016-09-02 11:10:14 -0400

    Allow colon or equal sign to be delimiter in ini_file

--- files/ini_file.py
+++ files/ini_file.py
@@ -110,9 +110,9 @@ import os
 
 def match_opt(option, line):
   option = re.escape(option)
-  return re.match('%s( |\t)*=' % option, line) \
-    or re.match('# *%s( |\t)*=' % option, line) \
-    or re.match('; *%s( |\t)*=' % option, line)
+  return re.match('%s( |\t)*(=|:)' % option, line) \
+    or re.match('# *%s( |\t)*(=|:)' % option, line) \
+    or re.match('; *%s( |\t)*(=|:)' % option, line)
 
 # ==============================================================
 # match_active_opt
